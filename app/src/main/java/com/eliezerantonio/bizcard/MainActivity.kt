8f58382e2ee80c5d6ed4db6d04a2e1ec41584434
eliezerantonio <eliezerfernandoantonio@gmail.com>
2024-10-09 15:41:57 +0100

    add header with profile image

--- app/src/main/java/com/eliezerantonio/bizcard/MainActivity.kt
+++ app/src/main/java/com/eliezerantonio/bizcard/MainActivity.kt
@@ -4,20 +4,31 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.eliezerantonio.bizcard.ui.theme.BizCardTheme
@@ -49,17 +60,49 @@ fun CreateBizCard() {
                 .width(200.dp)
                 .height(390.dp)
                 .padding(12.dp),
-            colors = CardDefaults.cardColors(containerColor = Color.Green),
             shape = RoundedCornerShape(corner = CornerSize(15.dp)),
             elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
         ) {
+            Column(
+                modifier = Modifier
+                    .height(300.dp)
+                    .fillMaxWidth()
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Top,
+                horizontalAlignment = Alignment.CenterHorizontally
 
+            ) {
+                CreateImageProfile()
+                HorizontalDivider(
+//                    thickness = 12.dp
+                )
+            }
 
 
         }
 
     }
 
+}
+
+@Composable
+fun CreateImageProfile(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier
+            .size(150.dp)
+            .padding(5.dp),
+        shape = CircleShape,
+        border = BorderStroke(0.5.dp, Color.LightGray), tonalElevation = 4.dp,
+        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.profile_image),
+            contentDescription = "Profile image",
+            modifier = Modifier.size(135.dp),
+            contentScale = ContentScale.Crop
+        )
+
+    }
 }
 
 @Preview(showBackground = true)
