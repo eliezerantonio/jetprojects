3e5e1304170dfbd4f858f488ea37dc0374a46ddf
eliezerantonio <eliezerfernandoantonio@gmail.com>
2024-10-09 17:30:06 +0100

    Add feature for toggle show porfolio

--- app/src/main/java/com/eliezerantonio/bizcard/MainActivity.kt
+++ app/src/main/java/com/eliezerantonio/bizcard/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.eliezerantonio.bizcard
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,20 +17,27 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.eliezerantonio.bizcard.ui.theme.BizCardTheme
@@ -50,6 +59,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBizCard() {
+
+    val buttonClickedState = remember { mutableStateOf(false) }
+
     Surface(
         modifier = Modifier
             .fillMaxWidth()
@@ -73,16 +85,98 @@ fun CreateBizCard() {
 
             ) {
                 CreateImageProfile()
-                HorizontalDivider(
-//                    thickness = 12.dp
-                )
-            }
+                HorizontalDivider()
+                CreateInfo()
+                Button(onClick = {
 
+                    Log.d("Clicked", "CreateBizCard: Clicked!!")
+
+                    buttonClickedState.value = !buttonClickedState.value
+                }) {
+
+                    Text("Portfolio", style = MaterialTheme.typography.bodySmall)
+                }
+
+                if (buttonClickedState.value) {
+                    Content()
+                }else {
+                    Box () {  }
+                }
+
+
+            }
 
         }
 
     }
 
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Content() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .padding(5.dp)
+    ) {
+        Surface(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .padding(3.dp),
+            shape = RoundedCornerShape(corner = CornerSize(6.dp)),
+            border = BorderStroke(width = 2.dp, color = Color.LightGray)
+        ) {
+
+            Portfolio(
+                data = listOf(
+                    "Project1",
+                    "Project2",
+                    "Project3",
+                    "Project4",
+                    "Project5",
+                    "Project6",
+                    "Project7",
+                    "Project8"
+                )
+            )
+        }
+
+    }
+
+
+}
+
+@Composable
+fun Portfolio(data: List<String>) {
+    LazyColumn() {
+        items(data) { item ->
+            Text(item)
+
+        }
+
+    }
+
+}
+
+@Composable
+private fun CreateInfo() {
+    Column(modifier = Modifier.padding(5.dp)) {
+        Text(
+            text = "Eliezer António",
+            style = MaterialTheme.typography.headlineSmall,
+            color = MaterialTheme.colorScheme.primary,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = "Mobile Developer",
+            style = MaterialTheme.typography.labelMedium,
+        )
+        Text(text = "eliezer@gmail.com", style = MaterialTheme.typography.labelMedium)
+
+    }
 }
 
 @Composable
@@ -92,7 +186,8 @@ fun CreateImageProfile(modifier: Modifier = Modifier) {
             .size(150.dp)
             .padding(5.dp),
         shape = CircleShape,
-        border = BorderStroke(0.5.dp, Color.LightGray), tonalElevation = 4.dp,
+        border = BorderStroke(0.5.dp, Color.LightGray),
+        tonalElevation = 4.dp,
         color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)
     ) {
         Image(
@@ -105,7 +200,7 @@ fun CreateImageProfile(modifier: Modifier = Modifier) {
     }
 }
 
-@Preview(showBackground = true)
+
 @Composable
 fun GreetingPreview() {
     BizCardTheme {
